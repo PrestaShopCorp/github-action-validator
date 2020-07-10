f98f38e330c91a095769634655e064387cde3156
Poulinho <poulai_a@etna-alternance.net>
2020-07-10 14:40:11 +0200

    debug params

--- script.php
+++ script.php
@@ -6,24 +6,31 @@ $client = new \GuzzleHttp\Client([
     "base_uri" => "https://validator.prestashop.com"
 ]);
 
-$response = $client->post('/api/modules', [
-    'multipart' => [
-        [
-            'name'     => 'github_link',
-            'contents' => $argv[1],
-        ],
-        [
-            'name'     => 'github_branch',
-            'contents' => $argv[2],
-        ],
-        [
-            'name'     => 'key',
-            'contents' => getenv('VALIDATOR_API_KEY')
+echo $argv[1];
+echo $argv[2];
+
+try {
+    $response = $client->post('/api/modules', [
+        'multipart' => [
+            [
+                'name'     => 'github_link',
+                'contents' => $argv[1],
+            ],
+            [
+                'name'     => 'github_branch',
+                'contents' => $argv[2],
+            ],
+            [
+                'name'     => 'key',
+                'contents' => getenv('VALIDATOR_API_KEY')
+            ]
         ]
-    ]
-]);
- 
-$body = $response->getBody()->getContents();
-$arr_body = json_decode($body);
-print_r($arr_body);
+    ]);
+     
+    $body = $response->getBody()->getContents();
+    $arr_body = json_decode($body);
+    var_dump($arr_body);
+} catch (\Throwable $th) {
+    var_dump($th->getMessage());
+}
   
